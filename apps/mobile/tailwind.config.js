1a1eb21c24f04d9983ce8865a7d4c365ae7d45d7
Ayush Gupta <ayushgupta3902@gmail.com>
2025-03-06 16:44:14 +0530

    chore: update folder and resolve errors

--- apps/mobile/tailwind.config.js
+++ apps/mobile/tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   content: [
     "./app/**/*.{js,jsx,ts,tsx}",
     "./components/**/*.{js,jsx,ts,tsx}",
-    "../../packages/ui/**/*.{js,jsx,ts,tsx}",
+    "../../packages/ui/src/**/*.{js,jsx,ts,tsx}",
   ],
   presets: [require("nativewind/preset")],
   theme: {
